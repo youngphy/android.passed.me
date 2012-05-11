40c8328a55b76b9f59e1fdbf59915966e9360e2f
sean.wang <xalinx@gmail.com>
2012-05-11 17:53:51 +0800

    program sort & cache r/w lock

--- src/me/passed/FeedActivity.java
+++ src/me/passed/FeedActivity.java
@@ -1,9 +1,14 @@
 package me.passed;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import android.app.Activity;
 import android.content.ContentResolver;
@@ -25,24 +30,26 @@ public class FeedActivity extends Activity {
 
 	private List<Integer> imgIdList;
 
-	private Map<Integer, Bitmap> imgCache;
+	private DisplayMetrics metrics;
 
-	private Cursor imgCursor;
+	private int scrollBarWidth = 0; // vertical scrollbar disabled
 
-	private Handler imgHandler;
+	private int columnWidth = 96;
+
+	private int gridSpacing = 0;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		long start = System.nanoTime();
-		
+
 		ContentResolver contentResolver = getContentResolver();
-		String[] projection = new String[] { MediaStore.Images.Media._ID, MediaStore.Images.Media.DATA };
-		imgCursor = contentResolver.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, projection, null, null, MediaStore.Images.Media.DATE_ADDED + " desc");
-		
+		String[] projection = new String[] { MediaStore.Images.Media._ID, MediaStore.Images.Media.DATA, MediaStore.Images.Media.DATE_ADDED };
+		Cursor imgCursor = contentResolver.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, projection, null, null, null);
+
 		Log.i("perf-init-query", "" + (System.nanoTime() - start));
-		
+
 		if (imgCursor != null) {
 			try {
 				imgCursor.moveToFirst();
@@ -61,25 +68,36 @@ public class FeedActivity extends Activity {
 				imgCursor.close();
 			}
 		}
-		
-		Log.i("perf-init-cache", "" + (System.nanoTime() - start));
-		
-		imgCache = new LinkedHashMap<Integer, Bitmap>() {
-			private static final long serialVersionUID = -7863147206244901890L;
+
+		Collections.sort(imgIdList, new Comparator<Integer>() {
 
 			@Override
-			protected boolean removeEldestEntry(Entry<Integer, Bitmap> eldest) {
-				return size() > 50;
+			public int compare(Integer arg0, Integer arg1) {
+				return arg1 - arg0;
 			}
 
-		};
+		});
 
-		imgHandler = new Handler();
+		Log.i("perf-init-idcache", "" + (System.nanoTime() - start));
+
+		metrics = new DisplayMetrics();
+		getWindowManager().getDefaultDisplay().getMetrics(metrics);
+
 		setContentView(R.layout.feed);
+
 		GridView g = (GridView) findViewById(R.id.feedGrid);
-		g.setColumnWidth(96);
-		g.setNumColumns(GridView.AUTO_FIT);
-		g.setVerticalSpacing(5);
+		g.setColumnWidth(columnWidth);
+		int numColumns = (metrics.widthPixels - scrollBarWidth + gridSpacing) / (columnWidth + gridSpacing);
+		g.setNumColumns(numColumns);
+		g.setVerticalSpacing(gridSpacing);
+		g.setHorizontalSpacing(gridSpacing);
+		int leftright = metrics.widthPixels - numColumns * columnWidth - (numColumns - 1) * gridSpacing;
+		int left = leftright / 2;
+		int right = leftright - left - scrollBarWidth;
+		g.setPadding(left, 0, right, 0);
+		g.setVerticalScrollBarEnabled(false);
+		g.setHorizontalScrollBarEnabled(false);
+
 		g.setAdapter(new ImageAdapter(this));
 
 		Log.i("perf-init-all", "" + (System.nanoTime() - start));
@@ -88,8 +106,30 @@ public class FeedActivity extends Activity {
 	public class ImageAdapter extends BaseAdapter {
 		private Context mContext;
 
+		private Map<Integer, Bitmap> imgCache;
+
+		private int cacheSize = 100;
+
+		private Handler imgHandler;
+
+		private ReadWriteLock rwLock = new ReentrantReadWriteLock();
+
+		private Lock rLock = rwLock.readLock();
+
+		private Lock wLock = rwLock.writeLock();
+
 		public ImageAdapter(Context c) {
 			mContext = c;
+			imgHandler = new Handler();
+			imgCache = new LinkedHashMap<Integer, Bitmap>() {
+				private static final long serialVersionUID = -7863147206244901890L;
+
+				@Override
+				protected boolean removeEldestEntry(Entry<Integer, Bitmap> eldest) {
+					return size() > cacheSize;
+				}
+
+			};
 		}
 
 		public int getCount() {
@@ -106,20 +146,25 @@ public class FeedActivity extends Activity {
 
 		public View getView(final int position, View convertView, ViewGroup parent) {
 			final ImageView imageView;
-			// int newWidth = 96;
-			// int newHeight = 96;
 			if (convertView == null) {
 				imageView = new ImageView(mContext);
-				// imageView.setLayoutParams(new GridView.LayoutParams(newWidth, newHeight));
-				// imageView.setAdjustViewBounds(false);
-				// imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-				// imageView.setPadding(0, 0, 0, 0);
+				imageView.setLayoutParams(new GridView.LayoutParams(columnWidth, columnWidth));
+				imageView.setAdjustViewBounds(false);
+				imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+				imageView.setPadding(0, 0, 0, 0);
 			} else {
 				imageView = (ImageView) convertView;
 			}
 
 			final int imgId = imgIdList.get(position);
-			Bitmap cache = imgCache.get(imgId);
+
+			Bitmap cache = null;
+			try {
+				rLock.lock();
+				cache = imgCache.get(imgId);
+			} finally {
+				rLock.unlock();
+			}
 			if (cache != null) {
 				// Log.d("cacheImg", String.format("pid:%s pos:%s cachesize:%s", imgId, position, imgCache.size()));
 				imageView.setImageBitmap(cache);
@@ -131,7 +176,12 @@ public class FeedActivity extends Activity {
 						Bitmap bm = MediaStore.Images.Thumbnails.getThumbnail(getContentResolver(), imgId, MediaStore.Images.Thumbnails.MICRO_KIND, null);
 						// Log.d("newImg", String.format("pid:%s pos:%s w:%s h:%s", imgId, position, bm.getWidth(), bm.getHeight()));
 						imageView.setImageBitmap(bm);
-						imgCache.put(imgId, bm);
+						try {
+							wLock.lock();
+							imgCache.put(imgId, bm);
+						} finally {
+							wLock.unlock();
+						}
 					}
 
 				});
@@ -139,7 +189,6 @@ public class FeedActivity extends Activity {
 
 			return imageView;
 		}
-
 	}
 
 }
